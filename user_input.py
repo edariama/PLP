42f50fe1afe288605bf44e6b9d681918b91899e9
edariama <160275659+edariama@users.noreply.github.com>
2024-02-28 20:43:17 +0300

    Initial Commit

--- user_input.py
+++ user_input.py
@@ -2,4 +2,4 @@ name = input("What is your name: ")
 age = int(input("What is your age: "))
 location = input("Where are you from: ")
 
-print(f"\nHello {name}, You are {age} years old and you come from '{location}'")
+print(f"Hello {name}, You are {age} years old and you come from '{location}'")
