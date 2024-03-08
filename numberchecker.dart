70b75e8a98b1612d080eb475bea22c1fbf5d1044
Gedaria Mandele <160275659+edariama@users.noreply.github.com>
2024-03-08 08:14:51 +0300

    Dart commits

--- numberchecker.dart
+++ numberchecker.dart
@@ -3,10 +3,10 @@ import 'dart:io';
 void main() {
   // Prompt the user for input
   print('Enter a number:');
-
+  
   // Read user input as a string and parse it into an integer
   int number = int.parse(stdin.readLineSync()!);
-
+  
   // Check the condition and print the appropriate message
   if (number > 10) {
     print('Your number is greater than 10');
@@ -15,3 +15,4 @@ void main() {
   } else {
     print('Your number is equal to 10');
   }
+}
